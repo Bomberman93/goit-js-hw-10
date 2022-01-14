291eea9768cf038159695bdee520d80a45a78839
Bomberman93 <nikita.bondarenko.1993@gmail.com>
2022-01-14 23:52:16 +0200

    dz10

--- src/index.js
+++ src/index.js
@@ -2,36 +2,50 @@ import './css/styles.css';
 import './fetchCountries';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const countrylistEl = document.querySelector('.country-list');
 const searchInputEl = document.querySelector('#search-box');
 const countryinfoEl = document.querySelector('.country-info');
+// const searchInputElVALue = searchInputEl.value.trim();
 
-searchInputEl.addEventListener("input", debounce(inputCountry, DEBOUNCE_DELAY));
-function inputCountry (event) {
-    // console.log(event.target.value.trim().length);
-    if (event.target.value.trim().length > 1 && event.target.value.trim().length < 10) {
+searchInputEl.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
 
-    } else {
-        // alert("Too many matches found. Please enter a more specific name.")
-    }
-};
-
-
-
-/*--------------Создает разметку для ЮЛ------------*/ 
-function randerCoutryCard (names) {
-    console.log(names);
-    const markup = names.map(name =>
-    {`<li><img src="${name.flags.svg}"/></li>`
-    countrylistEl.innerHTML = markup;
+function inputCountry(e) {
+  const search = e.target.value.trim();
+ 
+  fetchCountries(search)
+    .then(country => {
+      if (country.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        countrylistEl.innerHTML = '';
+        countryinfoEl.innerHTML = '';
+        return;
+      }
+      if (country.length >= 2 && country.length <= 10) {
+         const markup = country.map(coutr => {
+            return `<li class="item-card"><img src="${coutr.flags.svg}" alt="${coutr.name.official}" width="30" height="30"/>
+          ${coutr.name.official}</li>`;
+        }).join("");
+        countryinfoEl.innerHTML = '';
+        return countrylistEl.innerHTML = markup;
+      }
+      if (country.length === 1) {
+        const markup = country.map(coutr => {
+            return `<p class="item-card"><img src="${coutr.flags.svg}" alt="${
+              coutr.name.official
+            }" width="30" height="30"/>${coutr.name.official}</p>
+            <p>${coutr.capital}</p>
+            <p>${coutr.population}</p>
+            <p>${Object.values(coutr.languages)}</p>`;
+          }).join("");
+          countrylistEl.innerHTML = '';
+          return countryinfoEl.innerHTML = markup;
+      }
     })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
-randerCoutryCard(`Ukraine`);
 
-// fetchCountries(name).then(data => {
-// const markup = Object.entries(data).map(([key, value]) =>
-//   `<a href="${value}">${key}</a>`).join('');
-//   menuList.innerHTML = markup;
-// });
